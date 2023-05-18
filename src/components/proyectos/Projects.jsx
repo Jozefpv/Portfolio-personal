2a658b7203cd57eba52580da01197fb495bd8170
Jozefpv <jozefpv99@gmail.com>
2023-05-18 10:27:18 +0100

    cambio en el apartado de proyectos

--- src/components/proyectos/Projects.jsx
+++ src/components/proyectos/Projects.jsx
@@ -1,6 +1,7 @@
 import './style/styleProjects.css'
 import GifApp from './image/gifapp.jpg'
-// import BookingApp from './image/bookingApp.jpg'
+import BookingApp from './image/bookingApp.jpg'
+import TodoList from './image/todo.jpg'
 import Shop from './image/shop.jpg'
 
 function Projects() {
@@ -10,12 +11,19 @@ function Projects() {
       <p className=''></p>
       <div className='containerBoxGifApp'>
         <div className='boxGifApp'>
+          <a target='_blank' href='https://jozefpv.github.io/To-Do-List/' rel='noreferrer'>
+            <img className='photoGifApp' src={TodoList}></img>
+            <p className='textGifApp'>To do list</p>
+          </a>
+        </div>
+
+        <div className='boxGifApp'>
           <a target='_blank' href='https://jozefpv.github.io/React-Explorer/' rel='noreferrer'>
             <img className='photoGifApp' src={GifApp}></img>
             <p className='textGifApp'>GifApp</p>
           </a>
         </div>
-      
+
         <div className='boxGifApp'>
           <a target='_blank' href='https://jozefpv.github.io/React-Shop/' rel='noreferrer'>
             <img className='photoGifApp' src={Shop}></img>
@@ -23,12 +31,12 @@ function Projects() {
           </a>
         </div>
 
-        {/* <div className='boxGifAppNotAviable'>
+        <div className='boxGifAppNotAviable'>
           <a target='_blank' href='' rel='noreferrer'>
             <img className='photoGifApp' src={BookingApp}></img>
             <p className='textGifApp'>BookingApp</p>
           </a>
-        </div> */}
+        </div>
       </div>
     </div>
   )
